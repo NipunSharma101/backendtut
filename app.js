f46146a02ff522102e194e55c99c4fa2b07c54c5
unknown <nipunsharma26@yahoo.co.in>
2020-08-21 16:00:17 -0700

    running app with pug

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 const express = require("express") 
+const path = require("path")
 // the abouve line imports express into this app
 
 const app = express();
-const port = 4000;
+const port = 600;
 
-app.get("/", (req, res) => {
-    res.send("this is my first express app");
-});
+// this serves static files
+app.use('/static', express.static('static'))
 
-app.get("/ about", (req, res) => {
+//  set the template engine
+app.set('view engine', 'pug')
+
+// set the views directory
+app.set('views', path.join(__dirname, 'views'))
+
+// app.get("/", (req, res) => {
+//     res.send("this is my first express app");
+// });
+
+// our pug demo endpoint
+app.get('/demo',  (req, res) => {
+    res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
+  })
+
+app.get("/", (req,res)=>{
+      res.status(200).send("this is homepage of my first express app")
+  })
+
+// the below line will send post request and the above line will send get request
+
+app.get("/about", (req, res) => {
     res.send("this is my about express app");
 });
 
-app.get("/ contact", (req, res) => {
+app.get("/contact", (req, res) => {
     res.send("this is my contact express app");
 });
+
+// app.get("/this", (req, res) => {
+//     res.get("this page is not found. error 404.")
+// })
+
+// the line below will continuously listen to the commands above and will display on the port. act as a bridge between the two.
 app.listen(port, ()=>{
     console.log(`this application started successfully on ${port}`)
 })
