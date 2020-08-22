6558bdc19d8768f2a1f1fabdf0205cac4ea6d1be
unknown <nipunsharma26@yahoo.co.in>
2020-08-21 21:07:08 -0700

    first backend almost complete

--- app.js
+++ app.js
@@ -2,46 +2,28 @@ const express = require("express")
 const path = require("path")
 // the abouve line imports express into this app
 
+// EXPRESS SPECIFIC STUFF
 const app = express();
 const port = 600;
 
-// this serves static files
-app.use('/static', express.static('static'))
+// PUG SPECIFIC STUFF
+app.use('/static', express.static('static'))// this serves static files
 
-//  set the template engine
-app.set('view engine', 'pug')
 
-// set the views directory
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')//  set the template engine
 
-// app.get("/", (req, res) => {
-//     res.send("this is my first express app");
-// });
 
-// our pug demo endpoint
-app.get('/demo',  (req, res) => {
-    res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
-  })
+app.set('views', path.join(__dirname, 'views'))// set the views directory
 
-app.get("/", (req,res)=>{
-      res.status(200).send("this is homepage of my first express app")
-  })
+//ENDPOINT
+// we are sending this content written below to the html which is renderd online
+// this is the glitch in the matrix DARK, this connects both, the front end and the backend. 
+app.get('/', (req,res)=>{
+    const con = "this is the best content available in the market"
+    const params = {'title': 'pug is a good dog', "content": con}
+    res.status(200).render('index.pug', params)
+})
 
-// the below line will send post request and the above line will send get request
-
-app.get("/about", (req, res) => {
-    res.send("this is my about express app");
-});
-
-app.get("/contact", (req, res) => {
-    res.send("this is my contact express app");
-});
-
-// app.get("/this", (req, res) => {
-//     res.get("this page is not found. error 404.")
-// })
-
-// the line below will continuously listen to the commands above and will display on the port. act as a bridge between the two.
 app.listen(port, ()=>{
-    console.log(`this application started successfully on ${port}`)
+    console.log(`this application started successfully on ${port}`)// the line below will continuously listen to the commands above and will display on the port. act as a bridge between the two.
 })
