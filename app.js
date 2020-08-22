2b34df70e2e5bdeeb5e4ec4e738e90785f3bfb45
unknown <nipunsharma26@yahoo.co.in>
2020-08-22 16:39:45 -0700

    took input from front end and saved to deskto bEnd

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express") 
 const path = require("path")
 // the abouve line imports express into this app
-
-// EXPRESS SPECIFIC STUFF
+const fs = require("fs");
 const app = express();
 const port = 600;
 
-// PUG SPECIFIC STUFF
+
+
+// EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'))// this serves static files
+app.use(express.urlencoded()) //this will act as the cave that will get the data submitted on the frontend to us
+// the above line is called middleware and helps getting the form data to express.
 
-
+// PUG SPECIFIC STUFF
 app.set('view engine', 'pug')//  set the template engine
-
-
 app.set('views', path.join(__dirname, 'views'))// set the views directory
+
+
 
 //ENDPOINT
 // we are sending this content written below to the html which is renderd online
@@ -22,6 +25,19 @@ app.get('/', (req,res)=>{
     const con = "this is the best content available in the market"
     const params = {'title': 'pug is a good dog', "content": con}
     res.status(200).render('index.pug', params)
+
+})
+
+app.post('/', (req, res)=>{
+    // console.log(req.body)
+    name = req.body.name
+    age = req.body.age
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `name of the client is ${name} age ${age}, ${address}, ${more}`
+    fs.writeFileSync('output.txt', outputToWrite )
+    const params = {'message': 'your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
 })
 
 app.listen(port, ()=>{
